635ff861af41d2be42f330980f09771be6cdab9b
cheungzee <cheungzeecn@gmail.com>
2014-02-06 12:02:25 +0800

    add conf.py

---
+++ conf/conf.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-  
+# by zhangzhi @2014-02-05 21:25:56 
+# Copyright 2014 NONE rights reserved.
+
+
+iWorkThreadNum = 2
+
+if __name__ == '__main__':
+    print 'hello world'
+
